13c1019c1196b47703b1b6eec07365302d58522b
Kremez Dzmitry <kremez19@gmail.com>
2013-07-21 18:54:53 +0300

    Work Program

--- user_generator.rb
+++ user_generator.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 require 'ffaker'
 require 'ryba'
 
@@ -5,48 +6,68 @@ require 'ryba'
 lang = (ARGV[0])
 N = (ARGV[1]).to_i
 miss_perc = (ARGV[2])
-  
-@names = []
-@addresses = []
-@phone_numberes = []
-
-N.times do
-	if lang == "US"	
-      
-      @names << name 
-      address = "#{Faker::Address.city} #{Faker::Address.street_address} #{Random.rand(40)+1}"
-      @addresses << address
-      phone_number = "#{Faker::PhoneNumber.phone_number}"
-      @phone_numberes << phone_number
-      p "#{name}; #{address}; #{phone_number}."
-    elsif lang == "RU"
-      name = Ryba::Name.full_name
-      @names <<  name
-      address = Ryba::Address.postal
-      @addresses << address 
-      phone_number = Ryba::PhoneNumber.phone_number
-      @phone_numberes << phone_number
-      p "#{name}; #{address}; #{phone_number}."
-  end
-end
-
 
 def gen_us
   name = Faker::Name.name
-  address = "#{Faker::Address.city} #{Faker::Address.street_address} #{Random.rand(40)+1}"
+  address = "#{Faker::AddressUS.zip_code}, #{Faker::AddressUS.state}, #{Faker::Address.city} #{Faker::Address.street_address} h.#{Random.rand(40)+1}"
   phone_number = Faker::PhoneNumber.phone_number
-  us_user = "#{name}; #{address}; #{phone_number}."     
+  "#{name}; #{address}; #{phone_number}."    
 end
 
 def gen_ru
   name = Ryba::Name.full_name
   address = Ryba::Address.postal
   phone_number = Ryba::PhoneNumber.phone_number
-  ru_user = "#{name}; #{address}; #{phone_number}."     
+  "#{name}; #{address}; #{phone_number}."     
 end
 
-=begin
-p @name.to_s
-p @address
-p @phone_number.to_s
-=end
+def gen_by
+  name = Ryba::Name.full_name
+  address = by_postal
+  phone_number = by_phone_number
+  "#{name}; #{address}; #{phone_number}."     
+end
+  
+def by_postal
+  zip = index
+  "#{zip}, #{by_city(zip)}, #{Ryba::Address.address}"
+end
+
+def index
+  [220000,224000,210000,246000,230000,212000].sample
+end
+
+def by_phone_number
+  "+375 #{[29,33,44,25].sample} #{(0000000..9999999).to_a.sample}"
+end
+
+def by_city(zip) 
+  case zip
+      when 220000
+        'Минск'
+      when 210000
+        'Витебск'
+      when 246000
+        'Гомель'
+      when 230000
+        'Гродно'
+      when 212000
+        'Могилев'
+      when 224000
+        'Брест'
+      else
+        'Минск'
+  end
+end
+
+N.times do
+	if lang == "US"
+      p gen_us
+    elsif lang == "RU"
+      p gen_ru
+    elsif lang == "BY"
+      p gen_by 
+  end
+end
+
+
